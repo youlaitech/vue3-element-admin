3fa3d7ec0cc8c34c4efa38681e472f89ec7cf038
有来技术 <1490493387@qq.com>
2021-12-02 00:09:00 +0800

    fix(AppMain.vue): 首页无内容出现滚动条问题修复

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -36,17 +36,28 @@ export default defineComponent({
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .app-main {
-  /*50 = navbar  */
+  /* 50= navbar  50  */
   min-height: calc(100vh - 50px);
   width: 100%;
   position: relative;
   overflow: hidden;
 }
 
-.fixed-header + .app-main {
+.fixed-header+.app-main {
   padding-top: 50px;
+}
+
+.hasTagsView {
+  .app-main {
+    /* 84 = navbar + tags-view = 50 + 34 */
+    min-height: calc(100vh - 84px);
+  }
+
+  .fixed-header+.app-main {
+    padding-top: 84px;
+  }
 }
 </style>
 
