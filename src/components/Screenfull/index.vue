83b39f97c4296a7c22af26328dbf4c9df8b448c5
郝先瑞 <1490493387@qq.com>
2022-01-02 10:21:40 +0800

    refactor(Sceenfull): 移除无用引用exit

--- src/components/Screenfull/index.vue
+++ src/components/Screenfull/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <svg-icon :icon-class="isFullscreen ? 'exit-fullscreen' : 'fullscreen'" @click="toggle" />
+    <svg-icon :icon-class="isFullscreen ? 'exit-fullscreen' : 'fullscreen'" @click="toggle"/>
   </div>
 </template>
 
 <script setup>
-import { useFullscreen } from '@vueuse/core'
+import {useFullscreen} from '@vueuse/core'
 import SvgIcon from '@/components/SvgIcon/index.vue'
 
-const { isFullscreen, enter, exit, toggle } = useFullscreen();
+const {isFullscreen, enter, toggle} = useFullscreen();
 </script>
 
 <style lang='scss' scoped>
