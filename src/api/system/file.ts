075c7f88ba3e9e1cd676a948022caacfc5198907
郝先瑞 <1490493387@qq.com>
2022-01-08 18:40:49 +0800

    feat(file.ts): 添加文件上传接口

--- src/api/system/file.ts
+++ src/api/system/file.ts
@@ -1,5 +1,29 @@
 import request from '@/utils/request'
 
+/**
+ * 上传文件
+ *
+ * @param file
+ */
+export function uploadFile(file: any) {
+    let formData = new FormData()
+    formData.append('file', file)
+    return request(
+        {
+            url: '/youlai-admin/api/v1/files',
+            method: 'post',
+            data: formData,
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+        })
+}
+
+/**
+ * 删除文件
+ *
+ * @param path
+ */
 export function deleteFile(path: string) {
     return request({
         url: '/youlai-admin/api/v1/files',
