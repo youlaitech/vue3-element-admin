2f88df9a0f3be76708aeba72d025303d48f034c5
郝先瑞 <1490493387@qq.com>
2022-01-12 00:21:39 +0800

    feat(Link.vue): 如果设备是移动端，点击路由跳转同时关闭左侧菜单

--- src/layout/components/Sidebar/Link.vue
+++ src/layout/components/Sidebar/Link.vue
@@ -16,9 +16,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {computed, defineComponent} from 'vue'
 import { isExternal } from '@/utils/validate'
 import { useRouter } from 'vue-router'
+import {useAppStoreHook} from "@/store/modules/app";
+
+const sidebar = computed(() => useAppStoreHook().sidebar);
+const device = computed(() => useAppStoreHook().device);
+
 export default defineComponent({
   props: {
     to: {
@@ -29,7 +34,9 @@ export default defineComponent({
   setup(props) {
     const router = useRouter()
     const push = () => {
-      console.log(props.to)
+      if (device.value === 'mobile' && sidebar.value.opened == true) {
+        useAppStoreHook().closeSideBar(false)
+      }
       router.push(props.to).catch((err) => {
         console.log(err)
       })
