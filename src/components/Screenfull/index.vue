109299ae3409b94e8f42bc550aa48c4ce5274005
郝先瑞 <1490493387@qq.com>
2022-02-24 00:05:46 +0800

    style(Screenfull): 移除无用样式

--- src/components/Screenfull/index.vue
+++ src/components/Screenfull/index.vue
@@ -10,14 +10,3 @@ import SvgIcon from '@/components/SvgIcon/index.vue'
 
 const {isFullscreen, enter, toggle} = useFullscreen();
 </script>
-
-<style lang='scss' scoped>
-.screenfull-svg {
-  display: inline-block;
-  cursor: pointer;
-  fill: #5a5e66;
-  width: 20px;
-  height: 20px;
-  vertical-align: 10px;
-}
-</style>
