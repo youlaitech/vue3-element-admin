7cb45d05a02b88c840c75c7085f8be6c0b50d36a
郝先瑞 <1490493387@qq.com>
2022-02-24 00:00:57 +0800

    feat(i18n.ts): 国际化i18n工具类和动态路由国际化的方法generateTitle

---
+++ src/utils/i18n.ts
@@ -0,0 +1,12 @@
+// translate router.meta.title, be used in breadcrumb sidebar tagsview
+import i18n from "@/lang/index";
+
+export function generateTitle(title: any) {
+    // 判断是否存在国际化配置，如果没有原生返回
+    const hasKey = i18n.global.te('route.' + title)
+    if (hasKey) {
+        const translatedTitle = i18n.global.t('route.' + title)
+        return translatedTitle
+    }
+    return title
+}
