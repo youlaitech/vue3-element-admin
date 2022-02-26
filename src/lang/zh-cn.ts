209df5728402ca9d0d7ec6d0e6b5103800a2e408
郝先瑞 <1490493387@qq.com>
2022-02-27 01:04:30 +0800

    feat(zh-cn.ts): 完善登录页和导航页的国际化

--- src/lang/zh-cn.ts
+++ src/lang/zh-cn.ts
@@ -1,6 +1,24 @@
 export default {
     // 路由国际化
     route: {
-        dashboard: '首页'
+        dashboard: '首页',
+        document: '项目文档'
+    },
+    // 登录页面国际化
+    login:{
+        title: '有来商城管理系统Vue3',
+        username:'用户名',
+        password:'密码',
+        login:'登 录',
+        code:'请输入验证码',
+        copyright:'Copyright © 2021 - 2022 youlai.tech All Rights Reserved. 有来技术 版权所有',
+        icp:'ICP备案号:皖ICP备20006496号-2'
+    },
+    navbar:{
+        dashboard: '首页',
+        logout:'注销',
+        document:'项目文档',
+        gitee:'码云'
     }
+
 }
