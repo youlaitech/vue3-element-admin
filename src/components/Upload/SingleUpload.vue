83cea763f0b9a06eb884f8cdbe4c9b446ba97fb4
郝先瑞 <1490493387@qq.com>
2022-03-07 01:40:40 +0000

    fix: 删除多余引用

--- src/components/Upload/SingleUpload.vue
+++ src/components/Upload/SingleUpload.vue
@@ -35,7 +35,6 @@ import {computed, ref} from "vue";
 import {Plus, Close} from '@element-plus/icons-vue'
 import {ElMessage, ElUpload} from "element-plus"
 import {uploadFile, deleteFile} from "@/api/system/file";
-import {UploadFile} from "element-plus/es/components/upload/src/upload.type";
 
 const uploadRef = ref(ElUpload)
 const emit = defineEmits(['update:modelValue']);
