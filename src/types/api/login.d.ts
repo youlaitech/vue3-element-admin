ef0ee0150225f650c366c58c3481c1e56cb14aa1
郝先瑞 <1490493387@qq.com>
2022-03-13 15:46:42 +0800

    feat(login.d.ts): 添加登录API的TypeScript类型描述文件

--- src/types/api/login.d.ts
+++ src/types/api/login.d.ts
@@ -1,5 +1,5 @@
 /**
- * 登录请求参数
+ * 登录请求参数类型声明
  */
 export interface LoginRequestParam {
     username: string,
@@ -10,9 +10,18 @@ export interface LoginRequestParam {
 }
 
 /**
- * 登录响应参数
+ * 登录响应类型声明
  */
 export interface LoginResponseData {
     access_token: string,
     token_type: string
 }
+
+/**
+ * 验证码类型声明
+ */
+export interface Captcha {
+    img: string,
+    uuid: string
+}
+
