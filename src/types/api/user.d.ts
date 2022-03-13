eb66f6ec98a98d4edb3be98119c11fcbafdda578
郝先瑞 <1490493387@qq.com>
2022-03-13 15:49:04 +0800

    feat(user.d.ts): 添加用户API的TypeScript类型描述文件

--- src/types/api/user.d.ts
+++ src/types/api/user.d.ts
@@ -1,7 +1,7 @@
-import { Page, PageResult } from "./base"
+import { PageQueryParam, PageResult } from "./base"
 
 /**
- * 用户信息
+ * 用户信息类型声明
  */
 export interface UserInfo {
     nickname: string,
@@ -11,10 +11,52 @@ export interface UserInfo {
 }
 
 /**
- * 用户查询参数
+ * 用户查询参数类型声明
  */
-export interface UserQueryParam extends Page {
+export interface UserQueryParam extends PageQueryParam {
     keywords: String | undefined,
-    status: number | undefined ,
-    deptId: number | undefined 
+    status: number | undefined,
+    deptId: number | undefined
+}
+
+
+/**
+ * 用户分页列表项声明
+ */
+export interface UserItem {
+    id: string;
+    username: string;
+    nickname: string;
+    mobile: string;
+    gender: number;
+    avatar: string;
+    email: string;
+    status: number;
+    deptName: string;
+    roleNames: string;
+    gmtCreate: string;
+}
+
+/**
+ * 用户分页项类型声明
+ */
+export interface UserPageResult extends PageResult<UserItem[]> {
+
+}
+
+/**
+ * 用户表单类型声明
+ */
+export interface UserFormData {
+    id: number,
+    deptId: number,
+    username: string,
+    nickname: string,
+    password: string,
+    mobile: string,
+    email: string,
+    gender: number,
+    status: number,
+    remark: string,
+    roleIds: number[]
 }
