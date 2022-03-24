e19cc5a0ebec3fcfb23bb14bc236d080d8f380a7
郝先瑞 <1490493387@qq.com>
2022-03-24 23:52:02 +0800

    sytle(App.vue): 代码格式调整

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,16 @@ import { ElConfigProvider } from "element-plus";
 import { localStorage } from "@/utils/storage";
 import useStore from "@/store";
 
-//官方文档: https://element-plus.gitee.io/zh-CN/guide/i18n.html
-
 // 导入 Element Plus 语言包
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import en from "element-plus/es/locale/lang/en";
 
 const { app } = useStore();
+
 const language = computed(() => app.language);
 
 const locale = ref();
+
 watch(
   language,
   (value) => {
@@ -31,10 +31,11 @@ watch(
     }
   },
   {
-    // 初始化立即执行，
-    immediate: true,
+    // 初始化立即执行
+    immediate: true
   }
 );
+
 onMounted(() => {
   const style = localStorage.get("style");
   document.documentElement.style.cssText = style as string;
