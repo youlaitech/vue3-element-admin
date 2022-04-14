35d8ed2fdd67511679cf30a2da35410477eca5cc
郝先瑞 <1490493387@qq.com>
2022-04-15 00:39:41 +0800

    feat(eslintrc.js): 添加eslint代码检查配置

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    globals: {
+        defineProps: "readonly",
+        defineEmits: "readonly",
+        defineExpose: "readonly"
+    },
+    "parser": "vue-eslint-parser",
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "vue/multi-word-component-names": "off",
+        "@typescript-eslint/no-empty-function": "off", // 关闭空方法
+        "@typescript-eslint/no-explicit-any": "off", // 关闭any类型的警告
+        "vue/no-v-model-argument": "off"
+    }
+}
