04a8666fe06006c23b44b00ccc280be376c68793
郝先瑞 <1490493387@qq.com>
2022-04-24 15:05:03 +0800

    fix(types): 还原类型声明文件types
    Former-commit-id: 4a507ee6f133a5037de71d9fa3c1556379f5fdf5

---
+++ src/types/api/lab/seata.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Seata表单类型声明
+ */
+export interface SeataFormData {
+    openTx: boolean;
+    stockEx: boolean;
+    accountEx: boolean;
+    orderEx: boolean;
+}
---
+++ src/types/common.d.ts
@@ -0,0 +1,21 @@
+/**
+ * 组件类型声明
+ */
+
+/**
+ * 弹窗属性类型声明
+ */
+export interface Dialog {
+    title: string,
+    visible: boolean
+}
+
+/**
+ * 通用组件选择项类型声明
+ */
+export interface Option {
+    value: string,
+    label: string
+    children?: Option[]
+}
+
