4a507ee6f133a5037de71d9fa3c1556379f5fdf5
郝先瑞 <1490493387@qq.com>
2022-04-24 15:05:03 +0800

    fix(types): 还原类型声明文件types

---
+++ src/types/api/base.d.ts
@@ -0,0 +1,11 @@
+
+export interface PageQueryParam {
+    pageNum: number,
+    pageSize: number
+}
+
+export interface PageResult<T> {
+    list: T,
+    total: number
+}
+
---
+++ src/types/api/system/role.d.ts
@@ -0,0 +1,39 @@
+import { PageQueryParam, PageResult } from "../base"
+
+/**
+ * 角色查询参数类型声明
+ */
+export interface RoleQueryParam extends PageQueryParam {
+    name?: string
+}
+
+/**
+ * 角色分页列表项声明
+ */
+export interface RoleItem {
+    id: string;
+	name: string;
+	code: string;
+	sort: number;
+	status: number;
+	deleted: number;
+	menuIds?: any;
+	permissionIds?: any;
+}
+
+
+/**
+ * 角色分页项类型声明
+ */
+export type RolePageResult = PageResult<RoleItem[]>
+
+/**
+ * 角色表单类型声明
+ */
+export interface RoleFormData {
+    id: number|undefined,
+    name: string,
+    code: string,
+    sort: number,
+    status: number
+}
