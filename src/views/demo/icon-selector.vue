039dc1ce505489c4d1b360fd50771a10b5476227
haoxr <1490493387@qq.com>
2023-01-14 18:37:22 +0800

    refactor: 组件示例重构

---
+++ src/views/demo/icon-selector.vue
@@ -0,0 +1,11 @@
+<!-- 图标选择器 -->
+
+<script setup lang="ts">
+const value = ref('edit');
+</script>
+
+<template>
+  <div class="app-container">
+    <icon-select v-model="value" width="400px" />
+  </div>
+</template>
