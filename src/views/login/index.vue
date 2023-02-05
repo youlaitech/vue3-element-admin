f26577eff9ac0021bb5fd28d5ba243242a33cb67
haoxr <1490493387@qq.com>
2023-02-06 01:07:35 +0800

    refactor: 登录页面优化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,55 +4,50 @@
       ref="loginFormRef"
       :model="loginData"
       :rules="loginRules"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
+      class="w-[520px] mx-auto mt-[150px]"
     >
-      <div class="title-container">
-        <h3 class="title">{{ $t('login.title') }}</h3>
-        <lang-select class="set-language" />
+      <div class="flex text-white items-center py-4">
+        <span class="text-2xl flex-1 text-center">{{ $t('login.title') }}</span>
+        <lang-select style="color: #fff" />
       </div>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
+      <el-form-item prop="username" class="flex">
+        <span class="p-2 text-white">
           <svg-icon icon-class="user" />
         </span>
         <el-input
+          class="flex-1"
           ref="username"
+          size="large"
           v-model="loginData.username"
           :placeholder="$t('login.username')"
           name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
         />
       </el-form-item>
 
       <el-tooltip
-        :disabled="capslockTooltipDisabled"
+        :disabled="isCapslock === false"
         content="Caps lock is On"
         placement="right"
       >
-        <el-form-item prop="password">
-          <span class="svg-container">
+        <el-form-item class="flex" prop="password">
+          <span class="p-2 text-white">
             <svg-icon icon-class="password" />
           </span>
           <el-input
-            ref="passwordRef"
-            :key="passwordType"
+            class="flex-1"
             v-model="loginData.password"
-            :type="passwordType"
             placeholder="Password"
+            :type="passwordVisible === false ? 'password' : 'input'"
+            size="large"
             name="password"
-            tabindex="2"
-            auto-complete="on"
             @keyup="checkCapslock"
-            @blur="capslockTooltipDisabled = true"
             @keyup.enter="handleLogin"
           />
-          <span class="show-pwd" @click="showPwd">
+          <span class="mr-2" @click="passwordVisible = !passwordVisible">
             <svg-icon
-              :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+              :icon-class="passwordVisible === false ? 'eye' : 'eye-open'"
+              class="text-white cursor-pointer"
             />
           </span>
         </el-form-item>
@@ -62,32 +57,21 @@
         size="default"
         :loading="loading"
         type="primary"
-        style="width: 100%; margin-bottom: 30px"
+        class="w-full"
         @click.prevent="handleLogin"
         >{{ $t('login.login') }}
       </el-button>
 
       <!-- 账号密码提示 -->
-      <div class="tips">
-        <div style="position: relative">
-          <span style="margin-right: 20px"
-            >{{ $t('login.username') }}: admin</span
-          >
-          <span> {{ $t('login.password') }}: 123456</span>
-        </div>
+      <div class="mt-4 text-white text-sm">
+        <span>{{ $t('login.username') }}: admin</span>
+        <span class="ml-4"> {{ $t('login.password') }}: 123456</span>
       </div>
     </el-form>
-
-    <div v-if="showCopyright == true" class="copyright">
-      <p>{{ $t('login.copyright') }}</p>
-      <p>{{ $t('login.icp') }}</p>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, ref, toRefs, watch, nextTick } from 'vue';
-
 // 组件依赖
 import { ElForm, ElInput } from 'element-plus';
 import router from '@/router';
@@ -98,34 +82,27 @@ import SvgIcon from '@/components/SvgIcon/index.vue';
 import { useUserStore } from '@/store/modules/user';
 
 // API依赖
-import { useRoute } from 'vue-router';
-import { LoginData } from '@/api/auth/types';
+import { LocationQuery, LocationQueryValue, useRoute } from 'vue-router';
+import { LoginType } from '@/api/auth/types';
 
 const userStore = useUserStore();
 const route = useRoute();
 
 const loginFormRef = ref(ElForm);
-const passwordRef = ref(ElInput);
 
-const state = reactive({
-  redirect: '',
-  loginData: {
-    username: 'admin',
-    password: '123456'
-  } as LoginData,
-  loginRules: {
-    username: [{ required: true, trigger: 'blur' }],
-    password: [{ required: true, trigger: 'blur', validator: validatePassword }]
-  },
-  loading: false,
-  passwordType: 'password',
-  // 大写提示禁用
-  capslockTooltipDisabled: true,
-  otherQuery: {},
-  clientHeight: document.documentElement.clientHeight,
-  showCopyright: true,
-  showDialog: false
+const loginData = ref<LoginType>({
+  username: 'admin',
+  password: '123456'
 });
+
+const loginRules = {
+  username: [{ required: true, trigger: 'blur' }],
+  password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+};
+
+const passwordVisible = ref(false);
+
+const loading = ref(false);
 
 function validatePassword(rule: any, value: any, callback: any) {
   if (value.length < 6) {
@@ -135,30 +112,12 @@ function validatePassword(rule: any, value: any, callback: any) {
   }
 }
 
-const {
-  loginData,
-  loginRules,
-  loading,
-  passwordType,
-  capslockTooltipDisabled,
-  showCopyright
-} = toRefs(state);
+// 是否大写锁定
+const isCapslock = ref(false);
 
 function checkCapslock(e: any) {
   const { key } = e;
-  state.capslockTooltipDisabled =
-    key && key.length === 1 && key >= 'A' && key <= 'Z';
-}
-
-function showPwd() {
-  if (passwordType.value === 'password') {
-    passwordType.value = '';
-  } else {
-    passwordType.value = 'password';
-  }
-  nextTick(() => {
-    passwordRef.value.focus();
-  });
+  isCapslock.value = key && key.length === 1 && key >= 'A' && key <= 'Z';
 }
 
 /**
@@ -167,220 +126,59 @@ function showPwd() {
 function handleLogin() {
   loginFormRef.value.validate((valid: boolean) => {
     if (valid) {
-      state.loading = true;
+      loading.value = true;
       userStore
-        .login(state.loginData)
+        .login(loginData.value)
         .then(() => {
-          router.push({ path: state.redirect || '/', query: state.otherQuery });
-          state.loading = false;
+          const query: LocationQuery = route.query;
+
+          const redirect = (query.redirect as LocationQueryValue) ?? '/';
+
+          const otherQueryParams = Object.keys(query).reduce(
+            (acc: any, cur: string) => {
+              if (cur !== 'redirect') {
+                acc[cur] = query[cur];
+              }
+              return acc;
+            },
+            {}
+          );
+
+          router.push({ path: redirect, query: otherQueryParams });
         })
-        .catch(() => {
-          state.loading = false;
+        .finally(() => {
+          loading.value = false;
         });
-    } else {
-      return false;
     }
   });
 }
-
-watch(
-  route,
-  () => {
-    const query = route.query;
-    if (query) {
-      state.redirect = query.redirect as string;
-      state.otherQuery = getOtherQuery(query);
-    }
-  },
-  {
-    immediate: true
-  }
-);
-
-function getOtherQuery(query: any) {
-  return Object.keys(query).reduce((acc: any, cur: any) => {
-    if (cur !== 'redirect') {
-      acc[cur] = query[cur];
-    }
-    return acc;
-  }, {});
-}
-
-onMounted(() => {
-  window.onresize = () => {
-    if (state.clientHeight > document.documentElement.clientHeight) {
-      state.showCopyright = false;
-    } else {
-      state.showCopyright = true;
-    }
-  };
-});
 </script>
 
-<style lang="scss">
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
-
-/* reset element-ui css */
-.login-container {
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-
-    .set-language {
-      color: #fff;
-      position: absolute;
-      top: 3px;
-      font-size: 18px;
-      right: 0px;
-      cursor: pointer;
-    }
-  }
-
-  .el-input {
-    display: inline-block;
-    height: 36px;
-    width: 85%;
-    .el-input__wrapper {
-      padding: 0;
-      background: transparent;
-      box-shadow: none;
-      .el-input__inner {
-        background: transparent;
-        border: 0px;
-        -webkit-appearance: none;
-        border-radius: 0px;
-        color: $light_gray;
-        height: 36px;
-        caret-color: $cursor;
-
-        &:-webkit-autofill {
-          box-shadow: 0 0 0px 1000px $bg inset !important;
-          -webkit-text-fill-color: $cursor !important;
-        }
-      }
-    }
-  }
-
-  .el-input__inner {
-    &:hover {
-      border-color: var(--el-input-hover-border, var(--el-border-color-hover));
-      box-shadow: none;
-    }
-
-    box-shadow: none;
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-
-  .copyright {
-    width: 100%;
-    position: absolute;
-    bottom: 0;
-    font-size: 12px;
-    text-align: center;
-    color: #cccccc;
-  }
-}
-</style>
-
 <style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
+  background-color: #2d3a4b;
   overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 5px 10px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-
-  .captcha {
-    position: absolute;
-    right: 0;
-    top: 0;
-
-    img {
-      height: 42px;
-      cursor: pointer;
-      vertical-align: middle;
-    }
-  }
+}
+.el-form-item {
+  border: 1px solid #ffffff1a;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
 }
 
-.thirdparty-button {
-  position: absolute;
-  right: 40px;
-  bottom: 6px;
-}
-
-@media only screen and (max-width: 470px) {
-  .thirdparty-button {
-    display: none;
+.el-input {
+  background: transparent;
+  // 子组件 scoped 无效使用 :deep
+  :deep .el-input__wrapper {
+    padding: 0;
+    background: transparent;
+    box-shadow: none;
+    .el-input__inner {
+      color: #fff;
+      background: transparent;
+      caret-color: #fff;
+    }
   }
 }
 </style>
