e4b8f619c0fc1989f595c1440d948e192cc280b6
haoxr <1490493387@qq.com>
2023-02-06 01:04:56 +0800

    feat: 新增svg图标
    Former-commit-id: 7f2138a19e6907f944a03ee3493a9e7e4f7eca69

---
+++ src/views/demo/apidoc.vue
@@ -0,0 +1,14 @@
+<!-- 接口文档 -->
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-container h-full">
+    <iframe
+      id="iframe"
+      src="https://www.apifox.cn/apidoc/shared-195e783f-4d85-4235-a038-eec696de4ea5"
+      style="height: calc(100vh - 124px)"
+      width="100%"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
