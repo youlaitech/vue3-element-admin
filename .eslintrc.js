40eddea6994868acc0f3872a5086ecec1651010b
haoxr <1490493387@qq.com>
2023-03-11 20:44:42 +0800

    refactor:  移除 `defineProps` 等specifying-globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,12 @@
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
-  },
-  globals: {
-    defineProps: 'readonly',
-    defineEmits: 'readonly',
-    defineExpose: 'readonly',
-    DialogType: "readonly",
-    OptionType: "readonly",
+    node: true,
   },
   parser: 'vue-eslint-parser',
+   //  https://eslint.vuejs.org/user-guide/#bundle-configurations
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
@@ -20,8 +15,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
@@ -29,5 +24,10 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off', // 关闭空方法检查
     '@typescript-eslint/no-explicit-any': 'off', // 关闭any类型的警告
     'vue/no-v-model-argument': 'off'
+  },
+  // https://eslint.org/docs/latest/use/configure/language-options#specifying-globals
+  globals: {
+    DialogOption: 'readonly',
+    OptionType:'readonly'
   }
 };
