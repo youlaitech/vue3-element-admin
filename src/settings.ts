dd7e46f5b7be7406ee81f3af5ce47bd794813b7d
haoxr <1490493387@qq.com>
2023-03-12 21:35:20 +0800

    docs: 添加注释

--- src/settings.ts
+++ src/settings.ts
@@ -48,7 +48,13 @@ const defaultSettings: DefaultSettings = {
   fixedHeader: false,
   sidebarLogo: true,
   layout: 'left',
-  theme: 'dark', // dark | light
+  /**
+   *  主题模式
+   *
+   * dark:暗黑模式
+   * light: 明亮模式
+   */
+  theme: 'dark',
   size: 'default', // default |large |small
   language: 'zh-cn' // zh-cn| en
 };
