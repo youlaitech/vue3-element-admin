66499d06e91076c0d4a5ea0c37105547371ddde1
haoxr <1490493387@qq.com>
2023-03-23 07:42:48 +0800

    refactor: 全局注册自定义指令方式优化
    Former-commit-id: 40c40fa90d88d9efad4ef2fb73b00d650427fd05

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,1 +1,9 @@
-export { hasPerm, hasRole } from './permission';
+import type { App } from 'vue';
+
+import { hasPerm } from './permission';
+
+// 全局注册 directive
+export function setupDirective(app: App<Element>) {
+  // 使 v-hasPerm 在所有组件中都可用
+  app.directive('hasPerm', hasPerm);
+}
