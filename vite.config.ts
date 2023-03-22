9d17e600ed6ba2c9fc773059814d63db28a28db0
haoxr <1490493387@qq.com>
2023-03-22 22:19:27 +0800

    docs: 注释优化

--- vite.config.ts
+++ vite.config.ts
@@ -38,19 +38,17 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
         }
       }
     },
-    // 本地反向代理解决浏览器跨域限制
     server: {
       host: '0.0.0.0',
       port: Number(env.VITE_APP_PORT),
-      open: true, // 运行自动打开浏览器
+      open: true, // 运行是否自动打开浏览器
+      // 反向代理解决跨域
       proxy: {
         [env.VITE_APP_BASE_API]: {
-          // 线上API地址
           target: 'http://vapi.youlai.tech',
-          // 本地API地址
-          // target: 'http://localhost:8989',
           changeOrigin: true,
           rewrite: path =>
+            // localhost:3000/dev-api/users/me → http://vapi.youlai.tech/users/me
             path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '')
         }
       }
