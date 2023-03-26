8db6776de0b80ae65055e4321451049c2f19b7ee
haoxr <1490493387@qq.com>
2023-03-26 18:25:46 +0800

    refactor: 组件示例文件重命名
    Former-commit-id: a2072301b58e30a085a2637a34797117c8a8a1ce

---
+++ src/views/demo/IconSelect.vue
@@ -0,0 +1,10 @@
+<!-- 图标选择器示例 -->
+<script setup lang="ts">
+const iconName = ref('edit');
+</script>
+
+<template>
+  <div class="app-container">
+    <icon-select v-model="iconName" />
+  </div>
+</template>
