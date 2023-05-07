dc1a27b761d1a72fcdf7396793ca7ce2b515f045
郝先瑞 <1490493387@qq.com>
2023-05-07 21:07:51 +0800

    docs(vite.config.ts): :memo: 反向代理解决跨域注释完善

--- vite.config.ts
+++ vite.config.ts
@@ -40,17 +40,14 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       host: "0.0.0.0",
       port: Number(env.VITE_APP_PORT),
       open: true, // 运行是否自动打开浏览器
-      // 反向代理解决跨域
       proxy: {
+        // 反向代理解决跨域
         [env.VITE_APP_BASE_API]: {
-          // 线上接口API地址
-          target: "http://vapi.youlai.tech",
-          // 本地接口API地址
-          // target: 'http://localhost:8989',
+          target: "http://vapi.youlai.tech", // 线上接口地址
+          // target: 'http://localhost:8989',  // 本地接口地址 , 后端工程仓库地址：https://gitee.com/youlaiorg/youlai-boot
           changeOrigin: true,
           rewrite: (path) =>
-            // localhost:3000/dev-api/users/me → http://vapi.youlai.tech/users/me
-            path.replace(new RegExp("^" + env.VITE_APP_BASE_API), ""),
+            path.replace(new RegExp("^" + env.VITE_APP_BASE_API), ""), // 替换 /dev-api 为 target 接口地址
         },
       },
     },
