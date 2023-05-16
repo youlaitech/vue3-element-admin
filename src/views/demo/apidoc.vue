1a19253865a732b952bfa578eee8f47b727dfcbd
haoxr <1490493387@qq.com>
2023-05-17 04:34:09 +0800

    refactor: :recycle: 组件样式优化
    Former-commit-id: c2c03e83483b2f4646adb018859e82de6b60f1e1

--- src/views/demo/apidoc.vue
+++ src/views/demo/apidoc.vue
@@ -2,22 +2,18 @@
 <template>
   <div class="app-container">
     <iframe
-      id="apidoc-iframe"
       src="https://www.apifox.cn/apidoc/shared-195e783f-4d85-4235-a038-eec696de4ea5"
       width="100%"
+      height="100%"
       frameborder="0"
     ></iframe>
   </div>
 </template>
 
 <style lang="scss" scoped>
-#apidoc-iframe {
-  height: calc(100vh - 100px);
-}
-
-.hasTagsView {
-  #apidoc-iframe {
-    height: calc(100vh - 140px);
-  }
+.app-container {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 124px);
 }
 </style>
--- src/views/demo/wangEditor.vue
+++ src/views/demo/wangEditor.vue
@@ -1,11 +1,11 @@
 <!-- wangEditor富文本编辑器示例 -->
 <script setup lang="ts">
-import Editor from '@/components/WangEditor/index.vue';
-const value = ref('初始内容');
+import Editor from "@/components/WangEditor/index.vue";
+const value = ref("初始内容");
 </script>
 
 <template>
   <div class="app-container">
-    <editor v-model="value" style="height: 600px" />
+    <editor v-model="value" style="height: calc(100vh - 124px)" />
   </div>
 </template>
