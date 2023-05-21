a3547fed7b4f7cd851f7b696546e2dba654bba5e
haoxr <1490493387@qq.com>
2023-05-21 15:17:04 +0800

    feat: :sparkles: 组件封装示例添加源码地址

--- src/views/demo/icon-selector.vue
+++ src/views/demo/icon-selector.vue
@@ -5,6 +5,13 @@ const iconName = ref("edit");
 
 <template>
   <div class="app-container">
+    <el-link
+      href="https://gitee.com/youlaiorg/vue3-element-admin/blob/master/src/views/demo/icon-selector.vue"
+      type="primary"
+      target="_blank"
+      class="mb-10"
+      >示例源码 请点击>>>></el-link
+    >
     <icon-select v-model="iconName" />
   </div>
 </template>
--- src/views/demo/wang-editor.vue
+++ src/views/demo/wang-editor.vue
@@ -6,6 +6,13 @@ const value = ref("初始内容");
 
 <template>
   <div class="app-container">
-    <editor v-model="value" style="height: calc(100vh - 124px)" />
+    <el-link
+      href="https://gitee.com/youlaiorg/vue3-element-admin/blob/master/src/views/demo/wang-editor.vue"
+      type="primary"
+      target="_blank"
+      class="mb-[20px]"
+      >示例源码 请点击>>>></el-link
+    >
+    <editor v-model="value" style="height: calc(100vh - 180px)" />
   </div>
 </template>
