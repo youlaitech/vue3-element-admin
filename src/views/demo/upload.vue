7caf0f008ffcddafd26568e6fbd377420bdb9667
haoxr <1490493387@qq.com>
2023-05-24 07:44:45 +0800

    fix: :bug: 修改上传组件为外链图片，防止被删不显示
    Former-commit-id: 6776a8ef5c0cc785bd02ca4e07b3a032d8607cc8

--- src/views/demo/upload.vue
+++ src/views/demo/upload.vue
@@ -6,13 +6,13 @@ import MultiUpload from "@/components/Upload/MultiUpload.vue";
 const singlePicUrl = ref(
   "https://oss.youlai.tech/youlai-boot/2023/05/20/2b6d8b49fa1047348a0a41cef5aaf69e.gif"
 );
-
+// 这里放外链图片，防止被删
 const multiPicUrls = ref([
-  "https://oss.youlai.tech/youlai-boot/2023/05/20/7cc0e5bc14904bd4a8a5e25a7e47ae75.jpg",
-  "https://oss.youlai.tech/youlai-boot/2023/05/20/67ce044e457b4184a1ff5576e6672941.jpg",
-  "https://oss.youlai.tech/youlai-boot/2023/05/20/a64d0b4f45904dc7999ed620288d25d2.jpg",
-  "https://oss.youlai.tech/youlai-boot/2023/05/20/d94d0827c4974c279c190e9ba2b82070.jpg",
-  "https://oss.youlai.tech/youlai-boot/2023/05/20/e75cd4cbcfe54697bc14c42592427bfc.jpg",
+  "https://s2.loli.net/2023/05/24/yNsxFC8rLHMZQcK.jpg",
+  "https://s2.loli.net/2023/05/24/RuHFMwW4rG5lIqs.jpg",
+  "https://s2.loli.net/2023/05/24/ZPiGbcpR91WqInB.jpg",
+  "https://s2.loli.net/2023/05/24/e1bcnEq3MFdmlNL.jpg",
+  "https://s2.loli.net/2023/05/24/wZTSPj1yDQNcuhU.jpg",
 ]);
 </script>
 <template>
