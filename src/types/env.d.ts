3e30327e4d17f9b7a97dbf24c58006137ca32369
April <1317795103@qq.com>
2023-06-14 23:37:06 +0800

    feat: 表格排序
    Former-commit-id: 07383021b7a17b5beb30a8b6284478b5512c1743

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue';
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>;
   export default component;
@@ -17,3 +17,5 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare module "lodash-es";
