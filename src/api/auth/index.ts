ccdbaf7ce658fd967384450e6610714d5c0bcfb4
haoxr <1490493387@qq.com>
2023-06-24 23:50:23 +0800

    refactor: 登录表单参数从URL移入到请求体的FormData
    Former-commit-id: 474e69601fc257661fe8bed68f45c62990613cd0

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,6 +1,6 @@
-import request from '@/utils/request';
-import { AxiosPromise } from 'axios';
-import { CaptchaResult, LoginData, LoginResult } from './types';
+import request from "@/utils/request";
+import { AxiosPromise } from "axios";
+import { CaptchaResult, LoginData, LoginResult } from "./types";
 
 /**
  * 登录API
@@ -9,10 +9,18 @@ import { CaptchaResult, LoginData, LoginResult } from './types';
  * @returns
  */
 export function loginApi(data: LoginData): AxiosPromise<LoginResult> {
+  const formData = new FormData();
+  formData.append("username", data.username);
+  formData.append("password", data.password);
+  formData.append("verifyCodeKey", data.verifyCodeKey || "");
+  formData.append("verifyCode", data.verifyCode || "");
   return request({
-    url: '/api/v1/auth/login',
-    method: 'post',
-    params: data
+    url: "/api/v1/auth/login",
+    method: "post",
+    data: formData,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
   });
 }
 
@@ -21,19 +29,17 @@ export function loginApi(data: LoginData): AxiosPromise<LoginResult> {
  */
 export function logoutApi() {
   return request({
-    url: '/api/v1/auth/logout',
-    method: 'delete'
+    url: "/api/v1/auth/logout",
+    method: "delete",
   });
 }
-
-
 
 /**
  * 获取验证码
  */
 export function getCaptchaApi(): AxiosPromise<CaptchaResult> {
   return request({
-    url: '/api/v1/auth/captcha',
-    method: 'get'
+    url: "/api/v1/auth/captcha",
+    method: "get",
   });
 }
