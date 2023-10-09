244dc3fdff7851fd6a2d37fc3ab26f8f4ccdaa52
郝先瑞 <1490493387@qq.com>
2023-10-09 21:03:31 +0800

    fix: :bug: unoCss 插件版本升级智能提示失效问题修复
    Former-commit-id: 71a83c07698996b1adf6ebef074e9e18eac48351

---
+++ uno.config.ts
@@ -0,0 +1,34 @@
+// uno.config.ts
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
+      // ...
+    },
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        // ...
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+});
