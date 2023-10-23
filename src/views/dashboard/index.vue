9e1dfb56abc17535ddd74771e7bbf8e23284207d
hxr <1490493387@qq.com>
2023-10-24 00:13:52 +0800

    refactor: :recycle: 控制台等样式优化
    Former-commit-id: cb97c545b456eafcfa9486c8be0888de6d95327b

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -115,10 +115,8 @@ orderCount.value = 2000;
     <el-row :gutter="40" class="mb-4">
       <el-col :xs="24" :sm="12" :lg="6" class="mb-4">
         <div class="data-box">
-          <div
-            class="text-[#40c9c6] hover:!text-white hover:bg-[#40c9c6] p-3 rounded"
-          >
-            <svg-icon icon-class="uv" size="3em" />
+          <div class="p-3 rounded">
+            <svg-icon icon-class="visit" size="3em" />
           </div>
           <div class="flex flex-col space-y-3">
             <div class="text-[var(--el-text-color-secondary)]">访问数</div>
@@ -132,9 +130,7 @@ orderCount.value = 2000;
       <!--消息数-->
       <el-col :xs="24" :sm="12" :lg="6" class="mb-4">
         <div class="data-box">
-          <div
-            class="text-[#36a3f7] hover:!text-white hover:bg-[#36a3f7] p-3 rounded"
-          >
+          <div class="p-3 rounded">
             <svg-icon icon-class="message" size="3em" />
           </div>
           <div class="flex flex-col space-y-3">
@@ -148,9 +144,7 @@ orderCount.value = 2000;
 
       <el-col :xs="24" :sm="12" :lg="6" class="mb-4">
         <div class="data-box">
-          <div
-            class="text-[#f4516c] hover:!text-white hover:bg-[#f4516c] p-3 rounded"
-          >
+          <div class="p-3 rounded">
             <svg-icon icon-class="money" size="3em" />
           </div>
           <div class="flex flex-col space-y-3">
@@ -163,10 +157,8 @@ orderCount.value = 2000;
       </el-col>
       <el-col :xs="24" :sm="12" :lg="6" class="mb-2">
         <div class="data-box">
-          <div
-            class="text-[#34bfa3] hover:!text-white hover:bg-[#34bfa3] p-3 rounded"
-          >
-            <svg-icon icon-class="shopping" size="3em" />
+          <div class="p-3 rounded">
+            <svg-icon icon-class="cart" size="3em" />
           </div>
           <div class="flex flex-col space-y-3">
             <div class="text-[var(--el-text-color-secondary)]">订单数</div>
