36aea4579718dcb7d4b44dfa331b60bd91c1c7f6
hxr <1490493387@qq.com>
2023-10-24 00:14:59 +0800

    feat: :sparkles: 新增 swagger、knife4j 接口文档
    Former-commit-id: 4132c35ec881531fd91ebd8d85e9bb7fad48b7cb

---
+++ src/views/demo/api/swagger.vue
@@ -0,0 +1,27 @@
+<!-- 接口文档 -->
+<template>
+  <div class="app-container">
+    <iframe
+      src="http://vapi.youlai.tech/swagger-ui.html"
+      width="100%"
+      height="100%"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+/** 关闭tag标签  */
+.app-container {
+  /* 50px = navbar = 50px */
+  height: calc(100vh - 50px);
+}
+
+/** 开启tag标签  */
+.hasTagsView {
+  .app-container {
+    /* 84px = navbar + tags-view = 50px + 34px */
+    height: calc(100vh - 84px);
+  }
+}
+</style>
