4732c2376774fbfb880db806765604f3d1ca6d16
cshaptx4869 <994774638@qq.com>
2024-03-05 21:03:49 +0800

    build: :package: 定义vite全局常量替换

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,19 @@ import mockDevServerPlugin from "vite-plugin-mock-dev-server";
 
 import UnoCSS from "unocss/vite";
 import { resolve } from "path";
+import {
+  name,
+  version,
+  engines,
+  dependencies,
+  devDependencies,
+} from "./package.json";
+
+/** 平台的名称、版本、运行所需的`node`版本、依赖、构建时间的类型提示 */
+const __APP_INFO__ = {
+  pkg: { name, version, engines, dependencies, devDependencies },
+  buildTimestamp: Date.now(),
+};
 
 const pathSrc = resolve(__dirname, "src");
 //  https://cn.vitejs.dev/config
@@ -216,6 +229,9 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
           },
         },
       },
+    },
+    define: {
+      __APP_INFO__: JSON.stringify(__APP_INFO__),
     },
   };
 });
