af54fb26b6e51580d30c77d9db01b0e38d9eb1c2
cshaptx4869 <994774638@qq.com>
2024-05-30 14:16:39 +0800

    refactor: :recycle: 统一注册vue插件

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,24 @@
-export * from "./icons";
-export * from "./i18n";
-export * from "./permission";
+import { setupDirective } from "@/directive";
+import { setupI18n } from "@/lang";
+import { setupRouter } from "@/router";
+import { setupStore } from "@/store";
+import type { App } from "vue";
+import { setupElIcons } from "./icons";
+import { setupPermission } from "./permission";
+
+export default {
+  install(app: App<Element>) {
+    // 自定义指令(directive)
+    setupDirective(app);
+    // 路由(router)
+    setupRouter(app);
+    // 状态管理(store)
+    setupStore(app);
+    // 国际化
+    setupI18n(app);
+    // Element-plus图标
+    setupElIcons(app);
+    // 路由守卫
+    setupPermission();
+  },
+};
