0878f4b845f52f93cdcd1609f8c922baddbef922
cshaptx4869 <994774638@qq.com>
2024-05-31 09:15:53 +0800

    refactor: :recycle: 返回顶部颜色跟随主题色

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -30,7 +30,7 @@
         <Settings v-if="defaultSettings.showSettings" />
         <!-- 返回顶部 -->
         <el-backtop target=".main-container">
-          <svg-icon icon-class="backtop" color="#000000" size="24px" />
+          <svg-icon icon-class="backtop" size="24px" />
         </el-backtop>
       </div>
     </div>
@@ -45,7 +45,7 @@
       <Settings v-if="defaultSettings.showSettings" />
       <!-- 返回顶部 -->
       <el-backtop target=".main-container">
-        <svg-icon icon-class="backtop" color="#000000" size="24px" />
+        <svg-icon icon-class="backtop" size="24px" />
       </el-backtop>
     </div>
   </div>
