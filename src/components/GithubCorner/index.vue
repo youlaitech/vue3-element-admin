a611ab8b7582674c9642fed97bda1bd64a53e874
ray <1490493387@qq.com>
2024-07-07 20:30:52 +0800

    chore: :hammer: 修改Github仓库地址

--- src/components/GithubCorner/index.vue
+++ src/components/GithubCorner/index.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    href="https://github.com/haoxianrui"
+    href="https://github.com/youlaitech"
     target="_blank"
     class="github-corner"
     aria-label="View source on Github"
