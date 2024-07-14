6c0d50edb4b67e3dbd0c3d9d52d3aeb31728eb8d
ray <1490493387@qq.com>
2024-07-14 23:31:06 +0800

    feat: :sparkles: 整合 codemirror-editor-vue3 代码编辑器组件

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,9 @@ import "element-plus/theme-chalk/dark/css-vars.css";
 import "@/styles/index.scss";
 import "uno.css";
 import "animate.css";
+import { InstallCodemirro } from "codemirror-editor-vue3";
 
 const app = createApp(App);
 app.use(setupPlugins);
+app.use(InstallCodemirro);
 app.mount("#app");
