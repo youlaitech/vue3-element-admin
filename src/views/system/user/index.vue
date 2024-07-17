6dc27ebb2132b2ecb6e65cbb8826a0ddcb1d2f94
ray <1490493387@qq.com>
2024-07-17 22:06:30 +0800

    fix: :bug: radio使用value替换label作为值属性

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -270,8 +270,8 @@
 
         <el-form-item label="状态" prop="status">
           <el-radio-group v-model="formData.status">
-            <el-radio :label="1">正常</el-radio>
-            <el-radio :label="0">禁用</el-radio>
+            <el-radio :value="1">正常</el-radio>
+            <el-radio :value="0">禁用</el-radio>
           </el-radio-group>
         </el-form-item>
       </el-form>
