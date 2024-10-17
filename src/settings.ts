6f609c49ae7e2019adb41953571447aa44deb863
ray <1490493387@qq.com>
2024-10-17 23:58:04 +0800

    docs: :memo: 完善设置注释

--- src/settings.ts
+++ src/settings.ts
@@ -5,21 +5,33 @@ import { LanguageEnum } from "./enums/LanguageEnum";
 
 const { pkg } = __APP_INFO__;
 
+// 检查用户的操作系统是否使用深色模式
 const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
 
 const defaultSettings: AppSettings = {
+  // 系统Title
   title: pkg.name,
+  // 系统版本
   version: pkg.version,
+  // 是否显示设置
   showSettings: true,
+  // 是否显示标签视图
   tagsView: true,
+  // 是否固定头部
   fixedHeader: true,
+  // 是否显示侧边栏Logo
   sidebarLogo: true,
+  // 布局方式，默认为左侧布局
   layout: LayoutEnum.LEFT,
+  // 主题，根据操作系统的色彩方案自动选择
   theme: mediaQueryList.matches ? ThemeEnum.DARK : ThemeEnum.LIGHT,
   size: SizeEnum.DEFAULT,
   language: LanguageEnum.ZH_CN,
+  // 主题颜色
   themeColor: "#4080FF",
+  // 是否开启水印
   watermarkEnabled: false,
+  // 水印内容
   watermarkContent: pkg.name,
 };
 
