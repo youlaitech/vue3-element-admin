2fc574a352e9696137142aff2723e777f1471d55
ray <1490493387@qq.com>
2024-11-08 07:04:30 +0800

    refactor: :recycle: 精简 eslint 规则

--- eslint.config.js
+++ eslint.config.js
@@ -32,13 +32,6 @@ export default [
       ...configPrettier.rules,
       ...pluginPrettier.configs.recommended.rules,
       "no-debug": "off", // 禁止 debugger
-      "no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ], // 允许未使用的变量，以 _ 开头的变量不检查
       "prettier/prettier": [
         "error",
         {
@@ -62,23 +55,13 @@ export default [
     },
     rules: {
       ...pluginTypeScript.configs.strict.rules,
-      "@typescript-eslint/ban-types": "off", // 禁止特定类型
-      "@typescript-eslint/no-redeclare": "error", // 禁止重复声明
-      "@typescript-eslint/ban-ts-comment": "off", // 禁止特定注释
-      "@typescript-eslint/no-explicit-any": "off", // 禁止使用 any
-      "@typescript-eslint/prefer-as-const": "warn", // 使用 as const 替代 as 'const'
-      "@typescript-eslint/no-empty-function": "off", // 禁止空函数
-      "@typescript-eslint/no-non-null-assertion": "off", // 禁止非空断言
-      "@typescript-eslint/no-import-type-side-effects": "error", // 禁止导入类型产生副作用
-      "@typescript-eslint/explicit-module-boundary-types": "off", // 显式函数返回类型
+      "@typescript-eslint/no-explicit-any": "off", // 允许使用 any
+      "@typescript-eslint/no-empty-function": "off", // 允许空函数
+      "@typescript-eslint/no-empty-object-type": "off", // 允许空对象类型
       "@typescript-eslint/consistent-type-imports": [
         "error",
         { disallowTypeAnnotations: false, fixStyle: "inline-type-imports" },
-      ], // 一致的类型导入
-      "@typescript-eslint/prefer-literal-enum-member": [
-        "error",
-        { allowBitwiseExpressions: true },
-      ], // 使用字面量枚举成员
+      ], // 统一类型导入风格
     },
   },
 
@@ -88,15 +71,15 @@ export default [
     rules: {
       "eslint-comments/no-unlimited-disable": "off",
       "unused-imports/no-unused-vars": "off",
+      "@typescript-eslint/ban-ts-comment": "off", // 允许使用 @ts-nocheck 注释
     },
   },
 
-  // JavaScript 配置（包含 commonjs）
+  // JavaScript (commonjs) 配置
   {
     files: ["**/*.?([cm])js"],
     rules: {
-      "@typescript-eslint/no-require-imports": "off", // 禁止 require
-      "@typescript-eslint/no-var-requires": "off", // 禁止 require
+      "@typescript-eslint/no-var-requires": "off", // 允许 require
     },
   },
 
@@ -106,7 +89,6 @@ export default [
     languageOptions: {
       parser: parserVue,
       parserOptions: {
-        extraFileExtensions: [".vue"],
         parser: "@typescript-eslint/parser",
         sourceType: "module",
       },
@@ -116,24 +98,14 @@ export default [
     },
     processor: pluginVue.processors[".vue"],
     rules: {
-      ...pluginVue.configs.base.rules, // Vue 基础配置
-      ...pluginVue.configs["vue3-essential"].rules, // Vue3 基础配置
-      ...pluginVue.configs["vue3-recommended"].rules, // Vue3 推荐配置
-      "no-undef": "off",
-      "no-unused-vars": "off",
-      "vue/no-v-html": "off",
-      "vue/require-default-prop": "off",
-      "vue/require-explicit-emits": "off",
-      "vue/multi-word-component-names": "off",
-      "vue/no-setup-props-reactivity-loss": "off",
+      ...pluginVue.configs["vue3-recommended"].rules,
+      "vue/no-v-html": "off", // 允许 v-html
+      "vue/require-default-prop": "off", // 允许没有默认值的 prop
+      "vue/multi-word-component-names": "off", // 关闭组件名称多词要求
       "vue/html-self-closing": [
         "error",
         {
-          html: {
-            void: "always",
-            normal: "always",
-            component: "always",
-          },
+          html: { void: "always", normal: "always", component: "always" },
           svg: "always",
           math: "always",
         },
