a99076a01d2ba418487acab57f9dd40c21fb2985
Ray.Hao <1490493387@qq.com>
2025-03-13 21:26:38 +0800

    fix: :bug: 补充菜单下拉列表 mock

--- mock/menu.mock.ts
+++ mock/menu.mock.ts
@@ -491,7 +491,7 @@ export default defineMock([
     },
   },
 
-  // 获取菜单树形列表
+  // 获取菜单树形表格列表
   {
     url: "menus",
     method: ["GET"],
@@ -1552,6 +1552,351 @@ export default defineMock([
           ],
         },
       ],
+      msg: "一切ok",
+    },
+  },
+
+  // 获取菜单树形下拉列表
+  {
+    url: "menus/options",
+    method: ["GET"],
+    body: {
+      code: "00000",
+      data: [
+        {
+          value: "1",
+          label: "系统管理",
+          children: [
+            {
+              value: "2",
+              label: "用户管理",
+              children: [
+                {
+                  value: "105",
+                  label: "用户查询",
+                },
+                {
+                  value: "31",
+                  label: "用户新增",
+                },
+                {
+                  value: "32",
+                  label: "用户编辑",
+                },
+                {
+                  value: "33",
+                  label: "用户删除",
+                },
+                {
+                  value: "88",
+                  label: "重置密码",
+                },
+                {
+                  value: "106",
+                  label: "用户导入",
+                },
+                {
+                  value: "107",
+                  label: "用户导出",
+                },
+              ],
+            },
+            {
+              value: "3",
+              label: "角色管理",
+              children: [
+                {
+                  value: "139",
+                  label: "角色查询",
+                },
+                {
+                  value: "70",
+                  label: "角色新增",
+                },
+                {
+                  value: "71",
+                  label: "角色编辑",
+                },
+                {
+                  value: "72",
+                  label: "角色删除",
+                },
+              ],
+            },
+            {
+              value: "4",
+              label: "菜单管理",
+              children: [
+                {
+                  value: "73",
+                  label: "菜单新增",
+                },
+                {
+                  value: "140",
+                  label: "菜单查询",
+                },
+                {
+                  value: "75",
+                  label: "菜单删除",
+                },
+                {
+                  value: "74",
+                  label: "菜单编辑",
+                },
+              ],
+            },
+            {
+              value: "5",
+              label: "部门管理",
+              children: [
+                {
+                  value: "76",
+                  label: "部门新增",
+                },
+                {
+                  value: "141",
+                  label: "部门查询",
+                },
+                {
+                  value: "77",
+                  label: "部门编辑",
+                },
+                {
+                  value: "78",
+                  label: "部门删除",
+                },
+              ],
+            },
+            {
+              value: "6",
+              label: "字典管理",
+              children: [
+                {
+                  value: "79",
+                  label: "字典新增",
+                },
+                {
+                  value: "142",
+                  label: "字典查询",
+                },
+                {
+                  value: "81",
+                  label: "字典编辑",
+                },
+                {
+                  value: "84",
+                  label: "字典删除",
+                },
+              ],
+            },
+            {
+              value: "117",
+              label: "系统日志",
+            },
+            {
+              value: "135",
+              label: "字典数据",
+              children: [
+                {
+                  value: "143",
+                  label: "字典数据查询",
+                },
+                {
+                  value: "136",
+                  label: "字典数据新增",
+                },
+                {
+                  value: "137",
+                  label: "字典数据编辑",
+                },
+                {
+                  value: "138",
+                  label: "字典数据删除",
+                },
+              ],
+            },
+            {
+              value: "120",
+              label: "系统配置",
+              children: [
+                {
+                  value: "121",
+                  label: "系统配置查询",
+                },
+                {
+                  value: "122",
+                  label: "系统配置新增",
+                },
+                {
+                  value: "123",
+                  label: "系统配置修改",
+                },
+                {
+                  value: "124",
+                  label: "系统配置删除",
+                },
+                {
+                  value: "125",
+                  label: "系统配置刷新",
+                },
+              ],
+            },
+            {
+              value: "126",
+              label: "通知公告",
+              children: [
+                {
+                  value: "127",
+                  label: "通知查询",
+                },
+                {
+                  value: "128",
+                  label: "通知新增",
+                },
+                {
+                  value: "129",
+                  label: "通知编辑",
+                },
+                {
+                  value: "130",
+                  label: "通知删除",
+                },
+                {
+                  value: "133",
+                  label: "通知发布",
+                },
+                {
+                  value: "134",
+                  label: "通知撤回",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: "118",
+          label: "系统工具",
+          children: [
+            {
+              value: "119",
+              label: "代码生成",
+            },
+          ],
+        },
+        {
+          value: "40",
+          label: "接口文档",
+          children: [
+            {
+              value: "41",
+              label: "Apifox",
+            },
+          ],
+        },
+        {
+          value: "26",
+          label: "平台文档",
+          children: [
+            {
+              value: "102",
+              label: "document",
+            },
+            {
+              value: "30",
+              label: "平台文档(外链)",
+            },
+          ],
+        },
+        {
+          value: "20",
+          label: "多级菜单",
+          children: [
+            {
+              value: "21",
+              label: "菜单一级",
+              children: [
+                {
+                  value: "22",
+                  label: "菜单二级",
+                  children: [
+                    {
+                      value: "23",
+                      label: "菜单三级-1",
+                    },
+                    {
+                      value: "24",
+                      label: "菜单三级-2",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: "36",
+          label: "组件封装",
+          children: [
+            {
+              value: "108",
+              label: "增删改查",
+            },
+            {
+              value: "109",
+              label: "列表选择器",
+            },
+            {
+              value: "37",
+              label: "富文本编辑器",
+            },
+            {
+              value: "38",
+              label: "图片上传",
+            },
+            {
+              value: "39",
+              label: "图标选择器",
+            },
+            {
+              value: "95",
+              label: "字典组件",
+            },
+          ],
+        },
+        {
+          value: "110",
+          label: "路由参数",
+          children: [
+            {
+              value: "111",
+              label: "参数(type=1)",
+            },
+            {
+              value: "112",
+              label: "参数(type=2)",
+            },
+          ],
+        },
+        {
+          value: "89",
+          label: "功能演示",
+          children: [
+            {
+              value: "97",
+              label: "Icons",
+            },
+            {
+              value: "90",
+              label: "Websocket",
+            },
+            {
+              value: "91",
+              label: "敬请期待...",
+            },
+          ],
+        },
+      ],
+
       msg: "一切ok",
     },
   },
