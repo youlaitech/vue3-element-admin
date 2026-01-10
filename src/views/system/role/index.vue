a24bb92ba8da7efb8cce9373fa87a480b81a47a4
Ray.Hao <1490493387@qq.com>
2026-01-10 19:38:51 +0800

    feat: 添加权限控制到角色分配按钮，确保只有具备相应权限的用户可以进行角色分配

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -59,6 +59,7 @@
         <el-table-column fixed="right" label="操作" width="220">
           <template #default="scope">
             <el-button
+              v-hasPerm="'sys:role:assign'"
               type="primary"
               size="small"
               link
@@ -139,7 +140,6 @@
           />
         </el-form-item>
       </el-form>
-
       <template #footer>
         <div class="dialog-footer">
           <el-button type="primary" @click="handleSubmit">确定</el-button>
@@ -203,7 +203,9 @@
       </el-tree>
       <template #footer>
         <div class="dialog-footer">
-          <el-button type="primary" @click="handleAssignPermSubmit">确定</el-button>
+          <el-button v-hasPerm="'sys:role:assign'" type="primary" @click="handleAssignPermSubmit">
+            确定
+          </el-button>
           <el-button @click="assignPermDialogVisible = false">取消</el-button>
         </div>
       </template>
